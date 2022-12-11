1c216b7cb86b38c5233e25b1fa7f104c32dc5d5d
Gustavo Mustafa Mello Hanini <gustavo-mmello@hotmail.com>
2022-12-11 04:49:47 -0300

    Creation of the pagination changing the bg of card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,41 @@
-function convertPokemonToli(pokemon) {
-    return `
-            <li class="pokemon">
-                <span class="number">#001</span>
-                <span class="name">${pokemon.name}</span>
+const pagination = document.querySelector('#pagination')
+const loadMoreButton = document.querySelector('#loadMore')
+const pokemonsList = document.querySelector('#pokemonsList')
+const maxRecords = 151
+const limitIni = 12
+const limit = 10
+let offset = 0
+function backgronudMultipleTypes(pokemonDetail) {
+    let size = pokemonDetail.length
+    if (size > 1) return `style="background: linear-gradient(135deg,${pokemonDetail.map((type) => `var(--${type})`)})"`
+}
 
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        console.log(pokemons)
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}" ${backgronudMultipleTypes(pokemon.types)}>
+                <span class="number">${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    <img src="${pokemon.photo}"
                         alt="${pokemon.name}">
                 </div>
             </li>
-    `
+    `).join('')
+        pokemonsList.innerHTML += newHtml
+    }).catch((error) => { console.error('error: ', error) })
 }
-const pokemonsList = document.querySelector('#pokemonsList')
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonsList.innerHTML += pokemons.map(convertPokemonToli).join('')
-}).catch((error) => { console.error('error: ', error) })
+loadPokemonItens(offset, limitIni)
+loadMoreButton.addEventListener('click', () => {
+    offset += limitIni
+    let qntRecordsNextPage = offset + limit
+    if (qntRecordsNextPage >= maxRecords) {
+        let newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentNode.removeChild(loadMoreButton);
+    } else loadPokemonItens(offset, limit)
+})
