5fd43f37fe6ee25da18961b2dd78dd630f894f65
Gustavo Mustafa Mello Hanini <gustavo-mmello@hotmail.com>
2022-12-11 04:46:41 -0300

    Carrying the pokemons and their details

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -1,14 +1,30 @@
 const pokeApi = {}
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+
+
+
+function convertPokemonDetailToPokemon(pokemonDetail) {
+  const pokemon = new Pokemon
+  pokemon.number = pokemonDetail.id
+  pokemon.name = pokemonDetail.name
+  const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+  const [type] = types
+  pokemon.types = types
+  pokemon.type = type
+  pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default
+  return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then(response => response.json())
+    .then(convertPokemonDetailToPokemon)
+}
+pokeApi.getPokemons = (offset = 0, limit = 1) => {
   const baseUrl = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
   return fetch(baseUrl)
     .then((response) => response.json())
-    .then(async data => {
-      const pokemons = data.results
-      for (const pokemon of pokemons) {
-        pokemon.data = await fetch(pokemon.url).then(res => res.json())
-      }
-      return pokemons
-    })
-    .catch((error) => { console.error(error) })
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailsRequests) => Promise.all(detailsRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
+    .catch((error) => { console.error(`error: ${error}`) })
 }
